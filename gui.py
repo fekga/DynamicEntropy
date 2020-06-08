6f0979880b64ab0ecf32aab7d694615313b1f9d4
Gergo Cserhati <cserhati.gergo@gmail.com>
2020-06-08 19:51:12 +0200

    * tmp

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@
 
 from core import Resource,Converter
 import loader
+# import svg, panel, document from main
 
 class Node:
     radius = 20
@@ -11,18 +12,23 @@ class Node:
         self.converter = converter
         self.position = pos
 
-    def draw(self):
+    def draw(self, panel):
         x,y = self.position
         circle = svg.circle(cx=x, cy=y, r=self.radius,stroke="black",stroke_width="2",fill=self.color)
         title = svg.text(converter.name, x=x, y=y, font_size=8,text_anchor="middle")
+        panel <= circle
+        panel <= title
 
 nodes = []
 x,y=0,0
 for conv in Converter.converters.values():
     node = Node(conv,(x,y))
     nodes.append(node)
+    x += 50
 
 print(nodes)
 
 def draw_nodes():
-    print('guji')
+    panel = document['panel']
+    # for node in nodes:
+        # node.draw(panel)
