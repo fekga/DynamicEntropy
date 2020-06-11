758162903a50454b8d9ee77bdd634d94502710ce
Gábor Fekete <gabor.balazs.fekete@gmail.com>
2020-06-11 22:44:38 +0200

    tooltip added (wip)

--- test/hud.py
+++ test/hud.py
@@ -2,22 +2,24 @@
 
 from browser import document, svg
 
-def create_tspan(text,x,dy='1em'):
-    tspan = svg.tspan(text,x=x,dy=dy)
-    Hud.tspans.append(tspan)
-    return tspan
+
 
 class Hud:
-    panel = document['panel']
-    rect = panel.parent.getBoundingClientRect()
+    tooltip = document['tooltip']
+    rect = tooltip.getBoundingClientRect()
     width,height = rect.width,rect.height
-    node_name = svg.text("", x=10, y=0, z=10, font_size=20,text_anchor="start")
+    node_name = svg.text("", x=10, y=0, z=10, transform="translate(10)", font_size=20,text_anchor="start")
 
-    panel <= node_name
+    tooltip <= node_name
 
     hover_node = None
     active_node = None
     tspans = []
+
+    def create_tspan(text,dy='1em'):
+        tspan = svg.tspan(text,x=0,dy=dy)
+        Hud.tspans.append(tspan)
+        return tspan
 
     def set_active(node):
         Hud.active_node = node if node != Hud.active_node else None
@@ -30,24 +32,21 @@ class Hud:
             node = Hud.active_node
         elif node == Hud.hover_node:
             Hud.hover_node = None
-            Hud.node_name.text = ''
-            for e in Hud.tspans:
-                del e
+            Hud.tspans.clear()
             return
         else:
             Hud.hover_node = node
-        Hud.tspans = []
-        
-        Hud.node_name.text = node.name
+        Hud.node_name.attrs['transform'] = f"translate({Hud.hover_node.position[0] + 30})"
+        Hud.node_name.attrs['y'] = Hud.hover_node.position[1] - 30
+        Hud.node_name <= Hud.create_tspan(node.name)
 
         if node.in_recipes:
-            Hud.node_name <= create_tspan('Needs:',x=10,dy=25)
+            Hud.node_name <= Hud.create_tspan('Needs:',dy=25)
             for in_recipe in node.in_recipes:
                 text = f'{in_recipe.resource.name} [>{in_recipe.min_amount}]: {in_recipe.amount}'
-                Hud.node_name <= create_tspan(text,x=20)
+                Hud.node_name <= Hud.create_tspan(text)
         if node.out_recipes:
-            Hud.node_name <= create_tspan('Produces:',x=10,dy=25)
+            Hud.node_name <= Hud.create_tspan('Produces:',dy=25)
             for out_recipe in node.out_recipes:
                 text = f'{out_recipe.resource.name}: {out_recipe.amount}'
-                Hud.node_name <= create_tspan(text,x=20)
-        Hud.node_name.attrs['y'] = Hud.height - 20 - (len(Hud.tspans)-2) * 20 - 50
+                Hud.node_name <= Hud.create_tspan(text)
