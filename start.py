308e5206b1debf9ab21c5fb2868028ac5297140c
Gábor Fekete <gabor.balazs.fekete@gmail.com>
2020-07-03 20:13:00 +0200

    translateZ trick moved to all svgs, fixed version label

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-label = subprocess.check_output(["git", "describe"]).strip().decode("utf-8")
+label = subprocess.check_output(["git", "describe","--abbrev=0"]).strip().decode("utf-8")
 
 with open('core/app_version.py','w') as f:
     f.write(f'version_label = "{str(label)}"')
