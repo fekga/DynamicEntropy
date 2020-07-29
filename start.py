d69d0845ea792063138ea1d85819774e4b6b9015
Gergo Cserhati <cserhati.gergo@gmail.com>
2020-07-29 23:29:06 +0200

    * start script without server

--- start.py
+++ start.py
@@ -1,9 +1,15 @@
 import os
+import sys
 import subprocess
 label = subprocess.check_output(["git", "describe","--tags","--abbrev=0"]).strip().decode("utf-8")
 
 with open('core/app_version.py','w') as f:
     f.write(f'version_label = "{str(label)}"')
 
+
 os.system('python3 -m brython --modules')
-os.system('python3 -m http.server')
+
+if len(sys.argv) > 1 and sys.argv[1] == "noServer":
+    pass
+else:
+    os.system('python3 -m http.server')
